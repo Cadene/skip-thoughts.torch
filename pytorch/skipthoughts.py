2e23bed0257ee7078760d08f5accfe9cf305d070
Remi Cadene <remi.cadene@icloud.com>
2017-04-19 23:22:12 +0200

    Add BayesianUniSkip & DropUniSkip (-> BayesianGRU & SequentialDropout)

--- pytorch/skipthoughts.py
+++ pytorch/skipthoughts.py
@@ -1,31 +1,44 @@
-import torch
-import torch.nn as nn
-from torch.autograd import Variable
 import os
 import sys
 import numpy
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.autograd import Variable
 from collections import OrderedDict
+
+from gru import BayesianGRU, GRU
+from sequential_dropout import SequentialDropout
 
 urls = {}
 urls['dictionary'] = 'http://www.cs.toronto.edu/~rkiros/models/dictionary.txt'
 urls['utable']     = 'http://www.cs.toronto.edu/~rkiros/models/utable.npy'
 urls['uni_skip']   = 'http://www.cs.toronto.edu/~rkiros/models/uni_skip.npz'
 
-class UniSkip(nn.Module):
 
-    def __init__(self, dir_st, vocab, process_lengths=False, dropout=0, save=True):
-        super(UniSkip, self).__init__()
-        self.dir_st  = dir_st
-        self.vocab   = vocab
-        self.process_lengths = process_lengths
+# class AbstractSkipthoughts(nn.Module):
+
+#     def __init__(self, dir_st, vocab):
+#         super(AbstractSkipthoughts, self).__init__()
+#         self.dir_st = dir_st
+#         self.vocab = vocab
+#         # Modules
+#         import ipdb; ipdb.set_trace()
+        
+
+
+class AbstractUniSkip(nn.Module):
+
+    def __init__(self, dir_st, vocab, save=True, dropout=0):
+        super(AbstractUniSkip, self).__init__()
+        self.dir_st = dir_st
+        self.vocab = vocab
+        self.save = save
         self.dropout = dropout
-        self.save    = save
-        # Modules
-        self.embedding = self._load_embedding() 
-        self.gru       = self._load_gru()
-        # Remove bias_ih_l0 (== zero all the time)
-        del self.gru._parameters['bias_ih_l0']
-        del self.gru._all_weights[0][2]
+        self.embedding = self._load_embedding()
+        self.rnn = self._load_rnn()
  
     def _load_dictionary(self):
         path_dico = os.path.join(self.dir_st, 'dictionary.txt')
@@ -45,7 +58,7 @@ class UniSkip(nn.Module):
         params = numpy.load(path_params, encoding='latin1') # to load from python2
         return params
  
-    def _load_gru_params(self):
+    def _load_rnn_params(self):
         path_params = os.path.join(self.dir_st, 'uni_skip.npz')
         if not os.path.exists(path_params):
             os.system('mkdir -p ' + self.dir_st)
@@ -53,6 +66,27 @@ class UniSkip(nn.Module):
         params = numpy.load(path_params, encoding='latin1') # to load from python2
         return params
  
+    def _load_embedding(self):
+        if self.save:
+            import hashlib
+            import pickle
+            # http://stackoverflow.com/questions/20416468/fastest-way-to-get-a-hash-from-a-list-in-python
+            hash_id = hashlib.sha256(pickle.dumps(self.vocab, -1)).hexdigest()
+            path = '/tmp/uniskip_embedding_'+str(hash_id)+'.pth'
+        if self.save and os.path.exists(path):
+            self.embedding = torch.load(path)
+        else:
+            self.embedding = nn.Embedding(num_embeddings=len(self.vocab)+1,
+                                          embedding_dim=620,
+                                          padding_idx=0) # -> first_dim = zeros
+            dictionary = self._load_dictionary()
+            parameters = self._load_emb_params()
+            state_dict = self._make_emb_state_dict(dictionary, parameters)
+            self.embedding.load_state_dict(state_dict)
+            if self.save:
+                torch.save(self.embedding, path)
+        return self.embedding
+
     def _make_emb_state_dict(self, dictionary, parameters):
         weight = torch.zeros(len(self.vocab)+1, 620) # first dim = zeros -> +1
         unknown_params = parameters[dictionary['UNK']]
@@ -72,75 +106,173 @@ class UniSkip(nn.Module):
                   .format(nb_unknown, len(dictionary)))
         return state_dict
  
-    def _make_gru_state_dict(self, p):
-        s = OrderedDict()
-        s['bias_ih_l0']   = torch.zeros(7200) # must stay equal to 0
-        s['bias_hh_l0']   = torch.zeros(7200) 
-        s['weight_ih_l0'] = torch.zeros(7200, 620)
-        s['weight_hh_l0'] = torch.zeros(7200, 2400)
-        s['weight_ih_l0'][:4800] = torch.from_numpy(p['encoder_W']).t()
-        s['weight_ih_l0'][4800:] = torch.from_numpy(p['encoder_Wx']).t()
-        s['bias_hh_l0'][:4800]   = torch.from_numpy(p['encoder_b'])
-        s['bias_hh_l0'][4800:]   = torch.from_numpy(p['encoder_bx'])
-        s['weight_hh_l0'][:4800] = torch.from_numpy(p['encoder_U']).t()
-        s['weight_hh_l0'][4800:] = torch.from_numpy(p['encoder_Ux']).t()             
-        return s
- 
-    def _load_embedding(self):
-        if self.save:
-            import hashlib
-            import pickle
-            # http://stackoverflow.com/questions/20416468/fastest-way-to-get-a-hash-from-a-list-in-python
-            hash_id = hashlib.sha256(pickle.dumps(self.vocab, -1)).hexdigest()
-            path = '/tmp/uniskip_embedding_'+str(hash_id)+'.pth'
-        if self.save and os.path.exists(path):
-            self.embedding = torch.load(path)
-        else:
-            self.embedding = nn.Embedding(num_embeddings=len(self.vocab)+1,
-                                      embedding_dim=620,
-                                      padding_idx=0) # -> first_dim = zeros
-            dictionary = self._load_dictionary()
-            parameters = self._load_emb_params()
-            state_dict = self._make_emb_state_dict(dictionary, parameters)
-            self.embedding.load_state_dict(state_dict)
-            if self.save:
-                torch.save(self.embedding, path)
-        return self.embedding
- 
-    def _load_gru(self):
-        self.gru = nn.GRU(input_size=620,
-                          hidden_size=2400,
-                          batch_first=True,
-                          dropout=self.dropout)
-        parameters = self._load_gru_params()
-        state_dict = self._make_gru_state_dict(parameters)
-        self.gru.load_state_dict(state_dict)
-        return self.gru
- 
     def _select_last(self, input, lengths):
-        x = Variable(input.data.new().resize_((input.size(0), input.size(2))))
-        for i in range(input.size(0)):
-            x[i] = input[i,lengths[i]-1]
-        return x
+        batch_size = input.size(0)
+        x = []
+        for i in range(batch_size):
+            x.append(input[i,lengths[i]-1].view(1, 2400))
+        output = torch.cat(x, 0)
+        return output
  
     def _process_lengths(self, input):
         max_length = input.size(1)
         lengths = list(max_length - input.data.eq(0).sum(1).squeeze())
         return lengths
 
+    def _load_rnn(self):
+        raise NotImplementedError
+
+    def _make_rnn_state_dict(self, p):
+        raise NotImplementedError
+
     def forward(self, input, lengths=None):
-        if lengths is None or self.process_lengths:
+        raise NotImplementedError
+    
+
+class UniSkip(AbstractUniSkip):
+
+    def __init__(self, dir_st, vocab, save=True, dropout=0.25):
+        super(UniSkip, self).__init__(dir_st, vocab, save, dropout)
+        # Remove bias_ih_l0 (== zero all the time)
+        # del self.gru._parameters['bias_hh_l0']
+        # del self.gru._all_weights[0][3]
+
+    def _load_rnn(self):
+        self.rnn = nn.GRU(input_size=620,
+                          hidden_size=2400,
+                          batch_first=True,
+                          dropout=self.dropout)
+        parameters = self._load_rnn_params()
+        state_dict = self._make_rnn_state_dict(parameters)
+        self.rnn.load_state_dict(state_dict)
+        return self.rnn
+
+    def _make_rnn_state_dict(self, p):
+        s = OrderedDict()
+        s['bias_ih_l0']   = torch.zeros(7200) 
+        s['bias_hh_l0']   = torch.zeros(7200) # must stay equal to 0
+        s['weight_ih_l0'] = torch.zeros(7200, 620)
+        s['weight_hh_l0'] = torch.zeros(7200, 2400)
+        s['weight_ih_l0'][:4800] = torch.from_numpy(p['encoder_W']).t()
+        s['weight_ih_l0'][4800:] = torch.from_numpy(p['encoder_Wx']).t()
+        s['bias_ih_l0'][:4800]   = torch.from_numpy(p['encoder_b'])
+        s['bias_ih_l0'][4800:]   = torch.from_numpy(p['encoder_bx'])
+        s['weight_hh_l0'][:4800] = torch.from_numpy(p['encoder_U']).t()
+        s['weight_hh_l0'][4800:] = torch.from_numpy(p['encoder_Ux']).t()             
+        return s
+
+    def forward(self, input, lengths=None):
+        if lengths is None:
             lengths = self._process_lengths(input)
         x = self.embedding(input)
-        x, hn = self.gru(x) # seq2seq
+        x, hn = self.rnn(x) # seq2seq
         if lengths:
             x = self._select_last(x, lengths)
         return x
+
+
+class DropUniSkip(AbstractUniSkip):
+
+    def __init__(self, dir_st, vocab, save=True, dropout=0.25):
+        super(DropUniSkip, self).__init__(dir_st, vocab, save, dropout)
+        # Modules
+        self.seq_drop_x = SequentialDropout(p=self.dropout)
+        self.seq_drop_h = SequentialDropout(p=self.dropout)
+ 
+    def _make_rnn_state_dict(self, p):
+        s = OrderedDict()
+        s['bias_ih']   = torch.zeros(7200) 
+        s['bias_hh']   = torch.zeros(7200) # must stay equal to 0
+        s['weight_ih'] = torch.zeros(7200, 620)
+        s['weight_hh'] = torch.zeros(7200, 2400)
+        s['weight_ih'][:4800] = torch.from_numpy(p['encoder_W']).t()
+        s['weight_ih'][4800:] = torch.from_numpy(p['encoder_Wx']).t()
+        s['bias_ih'][:4800]   = torch.from_numpy(p['encoder_b'])
+        s['bias_ih'][4800:]   = torch.from_numpy(p['encoder_bx'])
+        s['weight_hh'][:4800] = torch.from_numpy(p['encoder_U']).t()
+        s['weight_hh'][4800:] = torch.from_numpy(p['encoder_Ux']).t()             
+        return s
+ 
+    def _load_rnn(self):
+        self.rnn = nn.GRUCell(620, 2400)
+        parameters = self._load_rnn_params()
+        state_dict = self._make_rnn_state_dict(parameters)
+        self.rnn.load_state_dict(state_dict)
+        return self.rnn
+ 
+    def forward(self, input, lengths=None):
+        batch_size = input.size(0)
+        seq_length = input.size(1) 
+
+        if lengths is None:
+            lengths = self._process_lengths(input)
+
+        x = self.embedding(input)
+
+        hx = Variable(x.data.new().resize_((batch_size, 2400)).fill_(0))
+        output = []
+
+        for i in range(seq_length):
+
+            if self.dropout > 0:
+                input_gru_cell = self.seq_drop_x(x[:,i,:])
+                hx = self.seq_drop_h(hx)
+            else:
+                input_gru_cell = x[:,i,:]
+
+            hx = self.rnn(input_gru_cell, hx)
+            output.append(hx.view(batch_size, 1, 2400))
+
+        output = torch.cat(output, 1) # seq2seq
+
+        if lengths:
+            output = self._select_last(output, lengths)
+
+        return output
+
+
+class BayesianUniSkip(AbstractUniSkip):
+
+    def __init__(self, dir_st, vocab, save=True, dropout=0.25):
+        super(BayesianUniSkip, self).__init__(dir_st, vocab, save, dropout)
+
+    def _make_rnn_state_dict(self, p):
+        s = OrderedDict()
+        s['gru_cell.weight_ir.weight'] = torch.from_numpy(p['encoder_W']).t()[:2400]
+        s['gru_cell.weight_ii.weight'] = torch.from_numpy(p['encoder_W']).t()[2400:]
+        s['gru_cell.weight_in.weight'] = torch.from_numpy(p['encoder_Wx']).t()
+
+        s['gru_cell.weight_ir.bias'] = torch.from_numpy(p['encoder_b'])[:2400]
+        s['gru_cell.weight_ii.bias'] = torch.from_numpy(p['encoder_b'])[2400:]
+        s['gru_cell.weight_in.bias'] = torch.from_numpy(p['encoder_bx'])
+
+        s['gru_cell.weight_hr.weight'] = torch.from_numpy(p['encoder_U']).t()[:2400]
+        s['gru_cell.weight_hi.weight'] = torch.from_numpy(p['encoder_U']).t()[2400:]       
+        s['gru_cell.weight_hn.weight'] = torch.from_numpy(p['encoder_Ux']).t()             
+        return s
+ 
+    def _load_rnn(self):
+        self.rnn = BayesianGRU(620, 2400, dropout=self.dropout)
+        parameters = self._load_rnn_params()
+        state_dict = self._make_rnn_state_dict(parameters)
+        self.rnn.load_state_dict(state_dict)
+        return self.rnn
+ 
+    def forward(self, input, lengths=None):
+        if lengths is None:
+            lengths = self._process_lengths(input)
+        x = self.embedding(input)
+        x, hn = self.rnn(x) # seq2seq
+        if lengths:
+            x = self._select_last(x, lengths)
+        return x
+
+
  
 if __name__ == '__main__':
     dir_st = '/local/cadene/data/skip-thoughts'
     vocab = ['robots', 'are', 'very', 'cool', '<eos>', 'BiDiBu']
-    uniskip = UniSkip(dir_st, vocab)
+    uniskip = BayesianUniSkip(dir_st, vocab)
  
     # batch_size x seq_len
     input = Variable(torch.LongTensor([
@@ -148,14 +280,15 @@ if __name__ == '__main__':
         [6,1,2,3,3,4,5],
         [6,1,2,3,3,4,5]
     ]))
+    print(input.size())
     uniskip.eval()
 
     # batch_size x 2400
     output_seq2vec = uniskip(input, lengths=[5,7,7])
+    print(output_seq2vec.sum())
 
     # batch_size x 2400
-    uniskip.process_lengths = True
     output_seq2vec2 = uniskip(input)
 
-    # batch_size x seq_len x 2400
-    output_seq2seq = uniskip(input)
+    # # batch_size x seq_len x 2400
+    # output_seq2seq = uniskip(input)
--- pytorch/test.py
+++ pytorch/test.py
@@ -2,21 +2,40 @@ import os
 import numpy
 import torch
 from torch.autograd import Variable
-from skipthoughts import UniSkip
+import skipthoughts
 
 class Tester():
 
    @staticmethod
+   def launch_all_tests(uniskip):
+      print('Test: '+str(uniskip)+'\n')
+      Tester.launch_tests(uniskip)
+
+      #print('With lengths processed')
+      #Tester.launch_tests(uniskip)
+
+   @staticmethod
    def launch_tests(uniskip):
-      methods = [func for func in dir(Tester) if callable(getattr(Tester, func)) and 'test_' in func]
+      methods = [func for func in dir(Tester) if callable(getattr(Tester, func)) and 'test_' == func[:5]]
       for name in methods:
          print(name)
          getattr(Tester, name)(uniskip)
+         print()
 
    @staticmethod
    def eq(tensor1, tensor2):
       dist = torch.dist(tensor1, tensor2)
       if dist < 1e-5:
+         msg = 'success'
+      else:
+         print(dist)
+         msg = 'fail'
+      print(msg)
+
+   @staticmethod
+   def neq(tensor1, tensor2):
+      dist = torch.dist(tensor1, tensor2)
+      if dist > 1e-5:
          msg = 'success'
       else:
          print(dist)
@@ -63,7 +82,39 @@ class Tester():
       Tester.eq(output.data[0], output_gt[0,:2400])
 
    @staticmethod
+   def test_words_nolengths(uniskip):
+      input = Variable(torch.LongTensor(1,3))
+      input.data.zero_()
+      input.data[0,0] = 1
+      input.data[0,1] = 2
+      input.data[0,2] = 4
+      output = uniskip(input)
+      output_gt = torch.from_numpy(numpy.load(os.path.join(dir_test,'features_normFalse_eosFalse.npy')))
+      Tester.eq(output.data[0], output_gt[0,:2400])
+
+   @staticmethod
+   def bgru_test_words_dropout(uniskip):
+      input = Variable(torch.LongTensor(1,3))
+      input.data.zero_()
+      input.data[0,0] = 1
+      input.data[0,1] = 2
+      input.data[0,2] = 4
+
+      uniskip.rnn.set_dropout(0.75)
+      print(str(uniskip))
+      output = uniskip(input)
+      output_gt = torch.from_numpy(numpy.load(os.path.join(dir_test,'features_normFalse_eosFalse.npy')))
+      Tester.neq(output.data[0], output_gt[0,:2400])
+
+      # uniskip.rnn.set_dropout(1)
+      # output = uniskip(input)
+      # Tester.eq(output.data[0], output.data[0].clone().fill_(0))
+
+      uniskip.rnn.set_dropout(0)
+
+   @staticmethod
    def test_backprop(uniskip):
+      uniskip.zero_grad()
       input = Variable(torch.LongTensor(1,3))
       input.data.zero_()
       input.data[0,0] = 1
@@ -74,8 +125,11 @@ class Tester():
       loss.backward()
       saved_grad = [params.grad.data.clone() for params in uniskip.parameters()]
 
-      uniskip.zero_grad()
+      # test if backprop all the way
+      for idx, params in enumerate(uniskip.parameters()):
+         Tester.neq(params.grad.data.clone().fill_(0), params.grad.data)
 
+      uniskip.zero_grad()
       input_pad = Variable(torch.LongTensor(1,10))
       input_pad.data.zero_()
       input_pad.data[0,0] = 1
@@ -85,6 +139,8 @@ class Tester():
       loss_pad = output_pad.sum()
       loss_pad.backward()
 
+      # test if backprop works the same way
+      # on padded input and non padded input
       for idx, params in enumerate(uniskip.parameters()):
          Tester.eq(saved_grad[idx], params.grad.data)
 
@@ -94,21 +150,14 @@ if __name__ == '__main__':
    vocab = ['robots', 'are', 'very', 'cool', '<eos>', 'BiDiBu']
 
    path_uniskip = os.path.join(dir_st, 'uniskip.pth')
-   if False and os.path.exists(path_uniskip):
-      uniskip = torch.load(path_uniskip)
-      uniskip.eval()
-   else:
-      uniskip = UniSkip(dir_st, vocab)
-      uniskip.eval()
-      torch.save(uniskip, path_uniskip)
+   uniskip = skipthoughts.UniSkip(dir_st, vocab, dropout=0)
+   drop_uniskip = skipthoughts.DropUniSkip(dir_st, vocab, dropout=0)
+   bayes_uniskip = skipthoughts.BayesianUniSkip(dir_st, vocab, dropout=0)
 
    if not os.path.exists(dir_test):
       os.system('python2 theano/dump_features.py')
 
-   print('With lengths given')
-   Tester.launch_tests(uniskip)
-
-   print('With lengths processed')
-   uniskip.zero_grad()
-   uniskip.process_lengths = True
-   Tester.launch_tests(uniskip)
+   Tester.launch_all_tests(uniskip)
+   Tester.launch_all_tests(drop_uniskip)
+   Tester.launch_all_tests(bayes_uniskip)
+   Tester.bgru_test_words_dropout(bayes_uniskip)
